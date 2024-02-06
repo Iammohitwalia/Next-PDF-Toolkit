460ede87ad1ba488e0a53bd082292fcf9562a7de
SubhamK108 <subhamk.subs@gmail.com>
2024-02-07 00:07:53 +0530

    Added the Home page & a custom 404 page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,39 @@
-export default function Home() {
+import Link from "next/link";
+import { ReactElement } from "react";
+
+export default function Home(): ReactElement {
   return (
-    <div className="bg-[#181818] h-screen flex flex-col justify-center items-center text-center">
-      <div className="font-[monospace] font-[350] text-9xl mb-10">
-        <h1>Hello World!</h1>
-      </div>
-      <div className="font-[sans serif] text-4xl">
-        <p>From Subham K.&nbsp;<i className="fa-regular fa-face-smile-beam"></i></p>
-      </div>
-    </div>
+    <>
+      <main className="h-full flex flex-col justify-center items-center">
+        <div className="h-[8rem] flex flex-col justify-center items-center text-center mt-14 max-sm:mt-10 mx-12 text-6xl max-sm:text-5xl font-sans">
+          Welcome to the PDF Toolkit
+        </div>
+        <div className="h-[6rem] flex flex-col justify-center items-center mt-9 text-5xl max-sm:text-4xl font-sans">
+          Available Tools
+        </div>
+        <div className="h-[20rem] w-[26rem] max-sm:w-[23rem] flex flex-wrap flex-row justify-evenly items-center mt-6 text-3xl max-sm:text-2xl font-sans">
+          <Link href="/pdf-merger">
+            <button className="rounded-lg bg-[#05336E] hover:bg-[#04234D] hover:ring hover:ring-[#074DA6] text-gray-200 p-4 h-32 w-40 max-sm:h-32 max-sm:w-36">
+              PDF Merger
+            </button>
+          </Link>
+          <Link href="/pdf-extractor">
+            <button className="rounded-lg bg-[#05336E] hover:bg-[#04234D] hover:ring hover:ring-[#074DA6] text-gray-200 p-4 h-32 w-40 max-sm:h-32 max-sm:w-36">
+              PDF Page Extractor
+            </button>
+          </Link>
+          <Link href="/pdf-page-deleter">
+            <button className="rounded-lg bg-[#05336E] hover:bg-[#04234D] hover:ring hover:ring-[#074DA6] text-gray-200 p-4 h-32 w-40 max-sm:h-32 max-sm:w-36">
+              PDF Page Deleter
+            </button>
+          </Link>
+          <Link href="/image-to-pdf">
+            <button className="rounded-lg bg-[#05336E] hover:bg-[#04234D] hover:ring hover:ring-[#074DA6] text-gray-200 p-4 h-32 w-40 max-sm:h-32 max-sm:w-36">
+              Image To PDF
+            </button>
+          </Link>
+        </div>
+      </main>
+    </>
   );
 }
