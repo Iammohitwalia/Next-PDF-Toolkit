90a598505b24e17cd62bd0ece2504549140ad93f
SubhamK108 <subhamk.subs@gmail.com>
2024-02-24 06:21:14 +0530

    Added a new PDF Encryptor button in the home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,25 +11,30 @@ export default function Home(): ReactElement {
         <div className="h-[6rem] flex flex-col justify-center items-center mt-9 text-5xl max-sm:text-4xl font-sans">
           Available Tools
         </div>
-        <div className="h-[20rem] w-[26rem] max-sm:w-[23rem] flex flex-wrap flex-row justify-evenly items-center mt-6 text-3xl max-sm:text-2xl font-sans">
+        <div className="h-[21rem] w-[40rem] max-sm:h-[25.5rem] max-sm:w-[23rem] flex flex-wrap flex-row justify-center items-center mt-6 max-sm:mt-4 text-3xl max-sm:text-2xl font-sans">
           <Link href="/pdf-merger">
-            <button className="rounded-lg bg-[#05336E] hover:bg-[#04234D] hover:ring hover:ring-[#074DA6] text-gray-200 p-4 h-32 w-40 max-sm:h-32 max-sm:w-36">
+            <button className="m-5 max-sm:m-3 rounded-lg bg-[#05336E] hover:bg-[#04234D] hover:ring hover:ring-[#074DA6] text-gray-200 p-4 h-32 w-40 max-sm:h-[7rem] max-sm:w-[8.5rem]">
               PDF Merger
             </button>
           </Link>
           <Link href="/pdf-extractor">
-            <button className="rounded-lg bg-[#05336E] hover:bg-[#04234D] hover:ring hover:ring-[#074DA6] text-gray-200 p-4 h-32 w-40 max-sm:h-32 max-sm:w-36">
+            <button className="m-5 max-sm:m-3 rounded-lg bg-[#05336E] hover:bg-[#04234D] hover:ring hover:ring-[#074DA6] text-gray-200 p-4 h-32 w-40 max-sm:h-[7rem] max-sm:w-[8.5rem]">
               PDF Page Extractor
             </button>
           </Link>
           <Link href="/pdf-page-deleter">
-            <button className="rounded-lg bg-[#05336E] hover:bg-[#04234D] hover:ring hover:ring-[#074DA6] text-gray-200 p-4 h-32 w-40 max-sm:h-32 max-sm:w-36">
+            <button className="m-5 max-sm:m-3 rounded-lg bg-[#05336E] hover:bg-[#04234D] hover:ring hover:ring-[#074DA6] text-gray-200 p-4 h-32 w-40 max-sm:h-[7rem] max-sm:w-[8.5rem]">
               PDF Page Deleter
             </button>
           </Link>
           <Link href="/image-to-pdf">
-            <button className="rounded-lg bg-[#05336E] hover:bg-[#04234D] hover:ring hover:ring-[#074DA6] text-gray-200 p-4 h-32 w-40 max-sm:h-32 max-sm:w-36">
+            <button className="m-5 max-sm:m-3 rounded-lg bg-[#05336E] hover:bg-[#04234D] hover:ring hover:ring-[#074DA6] text-gray-200 p-4 h-32 w-40 max-sm:h-[7rem] max-sm:w-[8.5rem]">
               Image To PDF
+            </button>
+          </Link>
+          <Link href="/pdf-encryptor">
+            <button className="m-5 max-sm:m-3 rounded-lg bg-[#05336E] hover:bg-[#04234D] hover:ring hover:ring-[#074DA6] text-gray-200 p-4 h-32 w-40 max-sm:h-[7rem] max-sm:w-[8.5rem]">
+              PDF Encryptor
             </button>
           </Link>
         </div>
