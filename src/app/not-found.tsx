9e16aaec88080c18c8b775e48ae68f01d5053f0c
SubhamK108 <subhamk.subs@gmail.com>
2024-02-29 03:15:22 +0530

    The `Go To Home` button in the 404 page will now `replace` the browser history instead of adding to it.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,8 +5,8 @@ export default function NotFound(): ReactElement {
   return (
     <div className="h-screen flex flex-col justify-center items-center text-center font-[sans serif]">
       <h1 className="text-[2rem] max-sm:text-[1.5rem] mb-5 px-10">Sorry, there&apos;s nothing at this address.</h1>
-      <p className="text-4xl mb-5">☹️</p>
-      <Link href="/">
+      <p className="text-4xl mb-10">☹️</p>
+      <Link href="/" replace={true}>
         <button className="text-3xl max-sm:text-2xl rounded-lg bg-[#05336E] hover:bg-[#04234D] hover:ring hover:ring-[#074DA6] text-gray-200 p-2 h-[4.5rem] w-52 max-sm:h-16 max-sm:w-40">
           Go to Home
         </button>
