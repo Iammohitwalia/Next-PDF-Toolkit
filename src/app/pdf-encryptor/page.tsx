835e259787b6eaf8fa770df138932b342ed95688
SubhamK108 <subhamk.subs@gmail.com>
2024-03-19 03:51:39 +0530

    Improved the colors of the Disabled Submit Buttons in Light Mode.

--- src/app/pdf-encryptor/page.tsx
+++ src/app/pdf-encryptor/page.tsx
@@ -221,7 +221,7 @@ export default function PdfEncryptor(): ReactElement {
                 </div>
                 <div className="h-[6rem] max-sm:h-[5rem]">
                   <button
-                    className="text-3xl max-sm:text-2xl rounded-xl bg-green-700 dark:bg-green-900 hover:bg-green-900 dark:hover:bg-green-950 disabled:bg-zinc-700 dark:disabled:bg-zinc-800 hover:ring hover:ring-green-500 dark:hover:ring-green-700 disabled:ring-transparent text-gray-200 disabled:text-zinc-500 dark:disabled:text-zinc-600 p-2 h-[4.5rem] w-52 max-sm:h-16 max-sm:w-40"
+                    className="text-3xl max-sm:text-2xl rounded-xl bg-green-700 dark:bg-green-900 hover:bg-green-900 dark:hover:bg-green-950 disabled:bg-zinc-400 dark:disabled:bg-zinc-800 hover:ring hover:ring-green-500 dark:hover:ring-green-700 disabled:ring-transparent text-gray-200 disabled:text-zinc-300 dark:disabled:text-zinc-600 p-2 h-[4.5rem] w-52 max-sm:h-16 max-sm:w-40"
                     onClick={submitFile}
                     disabled={pdfEncryptorState.EncryptorValidator === "VALID" ? false : true}
                   >
