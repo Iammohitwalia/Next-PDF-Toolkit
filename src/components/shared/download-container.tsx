5718b0f69e4a338715a10461599415e6eb33e15d
SubhamK108 <subhamk.subs@gmail.com>
2024-03-21 00:46:06 +0530

    Tweaked some padding

--- src/components/shared/download-container.tsx
+++ src/components/shared/download-container.tsx
@@ -23,11 +23,11 @@ export default function DownloadContainer(props: DownloadContainerProps): ReactE
     <>
       <main className="h-full flex flex-col justify-center items-center">
         {props.ToolName.length >= 15 ? (
-          <div className="font-bold tracking-wide h-[8rem] flex flex-col justify-center items-center text-center max-sm:mb-6 mt-14 max-sm:mt-5 px-8 text-6xl max-sm:text-[2.5rem] font-sans">
+          <div className="font-bold tracking-wide h-[8rem] flex flex-col justify-center items-center text-center max-sm:mb-6 mt-14 max-sm:mt-5 px-10 text-6xl max-sm:text-[2.5rem] font-sans">
             {props.ToolName}
           </div>
         ) : (
-          <div className="font-bold tracking-wide h-[8rem] flex flex-col justify-center items-center text-center mt-14 max-sm:mt-5 px-8 text-6xl max-sm:text-[2.5rem] font-sans">
+          <div className="font-bold tracking-wide h-[8rem] flex flex-col justify-center items-center text-center mt-14 max-sm:mt-5 px-10 text-6xl max-sm:text-[2.5rem] font-sans">
             {props.ToolName}
           </div>
         )}
